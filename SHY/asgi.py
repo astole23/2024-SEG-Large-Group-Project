2f0e9f10f2fc738ee6e9ce17120e9bd53d3a683d
Isabela Santos <isabela@dellapiazza.com>
2025-01-21 14:24:18 +0000

    created project base

--- SHY/asgi.py
+++ SHY/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SHY.settings')
 
 application = get_asgi_application()
--- SHY/wsgi.py
+++ SHY/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for shy project.
+WSGI config for SHY project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shy.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SHY.settings')
 
 application = get_wsgi_application()
