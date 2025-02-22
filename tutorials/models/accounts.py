7bd805a336c56cdb5dd3bc5bf983a2e23c7694c3
Alicia Luong <alicialuong32@gmail.com>
2025-02-22 17:27:39 +0000

    Creating front-end and back-end branch

--- tutorials/models/accounts.py
+++ tutorials/models/accounts.py
@@ -1,4 +1,5 @@
 from django.db import models
+import random
 
 
 class Company(models.Model):
@@ -7,6 +8,25 @@ class Company(models.Model):
     password = models.CharField(max_length=255)  
     industry = models.CharField(max_length=255)   
     phone = models.CharField(max_length=20) 
+    location = models.CharField(
+        max_length=100, 
+        blank=True, 
+        null=True,
+        help_text="Optional: Enter the company's location (up to 100 characters)."
+    )
+    logo = models.ImageField(upload_to='company_logos/', null=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+
+    unique_id = models.PositiveIntegerField(unique=True, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.unique_id:
+            while True:
+                new_id = random.randint(10000000, 99999999)  # Generates a random 8-digit number
+                if not Company.objects.filter(unique_id=new_id).exists():
+                    self.unique_id = new_id
+                    break
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.company_name
@@ -17,6 +37,6 @@ class User(models.Model):
     last_name = models.CharField(max_length=255)  
     email = models.EmailField(unique=True)        
     phone = models.CharField(max_length=20)       
-    password = models.CharField(max_length=255)   
+    password = models.CharField(max_length=255)
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
