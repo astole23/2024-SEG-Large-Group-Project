847b0b738fb7c83f8f4865166dfd535b2d04917d
aztxnio <126000342+aztxnio@users.noreply.github.com>
2025-03-17 17:44:56 +0000

    Update forms.py
    created form for updating user's details

--- tutorials/forms.py
+++ tutorials/forms.py
@@ -145,3 +145,8 @@ class CVApplicationForm(forms.ModelForm):
     class Meta:
         model = CVApplication
         fields = '__all__'
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'industry', 'location')
