2b8349c60c0b6fb511086740d6652346bfdb5354
Isabela Santos <isabela@dellapiazza.com>
2025-03-24 14:40:01 +0000

    pull from main

--- tutorials/tests/test_function_views.py
+++ tutorials/tests/test_function_views.py
@@ -295,11 +295,18 @@ class AuthViewsTests(TestCase):
         response = self.client.post(reverse('delete_raw_cv'))
         self.assertEqual(response.status_code, 302)
 
-    def test_raw_cv_with_no_cv(self):
-        self.client.login(username='testcompany', password='testpass123')  # Company user has no CV
-        response = self.client.post(reverse('delete_raw_cv'))
-        self.assertEqual(response.status_code, 404)
+    def test_delete_raw_cv_post_no_cv(self):
+        self.client.login(username='user', password='testpass')
 
+        response = self.client.post(reverse('delete_raw_cv'))
+
+        print("Is authenticated:", response.wsgi_request.user.is_authenticated)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            "success": False,
+            "error": "No CV found."
+        })
 
     def test_delete_raw_cv_with_get_request(self):
         self.client.login(username='testuser', password='testpass123')
